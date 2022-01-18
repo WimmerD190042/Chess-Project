945cdbd474f72cb27a4bc481e3c6ec20a31f9d0a
WimmerD190042 <WimmerD190042@sus.htl-grieskirchen.at>
2022-01-18 15:50:28 +0100

    weird board

--- src/ChessBoardWithColumnsAndRows.java
+++ src/ChessBoardWithColumnsAndRows.java
@@ -1,21 +1,44 @@
-import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.net.ServerSocket;
+import java.util.LinkedList;
+import java.util.concurrent.Executors;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class ChessBoardWithColumnsAndRows extends JFrame {
+
+
+    public static final String uniCodeWhitePawn="\u2654";
 
     // The overall box count in chess board
     public static final int squareCount = 64;
 
     public ChessBoardWithColumnsAndRows(String title) {
 
+        LinkedList<Piece> pieceList = new LinkedList<>();
+
+        //die Pieces adden:
+
+        pieceList.add(new Piece(2,2,false,"pawn",pieceList));
+
+
+
+
+
+
+
         // Color of chess board this you can change accordingly like red or white
-        Color blackColor = Color.BLACK;
+        Color blackColor = Color.gray;
         Color whiteColor = Color.WHITE;
 
         JButton chessButton = null;
+
 
         for (int i = 1; i <= squareCount; i++) {
 
@@ -26,8 +49,12 @@ public class ChessBoardWithColumnsAndRows extends JFrame {
                 add(chessButton);
             } else {
 
-                chessButton = new JButton();
+                chessButton = new JButton("\u2659");
                 chessButton.setBackground(whiteColor);
+                //
+
+
+                //
                 add(chessButton);
             }
 
@@ -41,9 +68,22 @@ public class ChessBoardWithColumnsAndRows extends JFrame {
         }
 
         this.setTitle(title); // Setting the title of board
-        this.setLayout(new GridLayout(8, 18)); // GridLayout will arrange elements in Grid Manager 8 X 8
+        this.setLayout(new GridLayout(8, 8)); // GridLayout will arrange elements in Grid Manager 8 X 8
         this.setSize(650, 650); // Size of the chess board
         this.setVisible(true);
+
+
+        for(int zeile=0;zeile<8;zeile++) {
+
+           if(zeile==1){
+               for(int spalte=0;spalte<8;spalte++) {
+                   JButton button = new JButton("\u2659");
+               }
+           }
+        }
+
+
+
 
     }
 
@@ -52,5 +92,33 @@ public class ChessBoardWithColumnsAndRows extends JFrame {
         String title = "Schachbrett";
 
         ChessBoardWithColumnsAndRows chessBoard = new ChessBoardWithColumnsAndRows(title); // Creating the instance of chess board
+
+        String unicodeMessage =
+                "\u2654 " + // white king
+                        "\u2655 " + // white queen
+                        "\u2656 " + // white rook
+                        "\u2657 " + // white bishop
+                        "\u2658 " + // white knight
+                        "\u2659 " + // white pawn
+                        "\n" +
+                        "\u265A " + // black king
+                        "\u265B " + // black queen
+                        "\u265C " + // black rook
+                        "\u265D " + // black bishop
+                        "\u265E " + // black knight
+                        "\u265F " + // black pawn
+                        "\n" ;
+        try (PrintStream out = new PrintStream(System.out, true, "UTF8")) {
+            out.println(unicodeMessage);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+
+
+
+
 }
